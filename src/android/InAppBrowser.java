cf939065e68df935488a3e7882d885c3ef33747d
Roy Stijsiger <roy.stijsiger@outlook.com>
2020-06-05 14:23:07 +0200

    Woops forgot a )

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1316,7 +1316,7 @@ public class InAppBrowser extends CordovaPlugin {
                 }
                 if (allowedSchemes != null) {
                     for (String scheme : allowedSchemes) {
-                        if (url.contains('*'){
+                        if (url.contains('*')){
                             try {
                                 JSONObject obj = new JSONObject();
                                 obj.put("type", "customscheme");
